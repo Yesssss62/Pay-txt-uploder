17d8005e3b8a172a2e2d6167d0d45f594dfee13b
Yesssss62 <sunilkumar20122006@gmail.com>
2024-05-23 18:07:27 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "28528007"
-api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "7159007758:AAHGYgsZadZqkBLSe5IYrrmuhegMBHx1tiQ"
+api_id = "2511244"
+api_hash = "eec08d89fa5a627345966a73390231ad"
+bot_token = "6531753932:AAGMZyipxX11OG0VQDCT4lZ2mIiKcVtlMH0"
